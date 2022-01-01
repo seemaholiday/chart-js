d908bc2e3a0498ae9fa467d69711cf4e82232d96
seemaholiday <seemaholidaydeveloper@gmail.com>
2022-01-01 20:30:25 +0530

    latest commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Horizontalchart from './chart-horizontal'
+
+function App() {
+  return (
+    <div className="App">
+      <Horizontalchart/>
+    </div>
+  );
+}
+
+export default App;
